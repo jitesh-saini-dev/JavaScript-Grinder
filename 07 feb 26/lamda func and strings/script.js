ff88667ed824201ff6d0a39830f2c467bbf37501
jitesh <iamjiteshsaini2004@gmail.com>
2026-02-09 15:38:59 +0530

    updated and added file

---
+++ 07 feb 26/lamda func and strings/script.js
@@ -0,0 +1,8 @@
+// Write a lambda function to return the sum of two numbers.
+
+// let sum = (a, b) => a + b;
+// console.log(sum(5, 6)); 
+
+// Write a lambda function to check if a number is even. 
+
+// let check =(n)=>if(n%2)

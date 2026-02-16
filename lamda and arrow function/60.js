2bf057989a8ba40eb7d514f243f87f10e2ead842
jitesh <iamjiteshsaini2004@gmail.com>
2026-02-17 00:24:35 +0530

    updated 60th

--- lamda and arrow function/60.js
+++ lamda and arrow function/60.js
@@ -1,3 +1,14 @@
 // 60. Palindrome Number
-const isPal = n => String(n) === String(n).split('').reverse().join('');
-console.log(isPal(121));
+const isPalNum = (n) => {
+  let rev = 0;
+  let temp = n;
+
+  while (temp > 0) {
+    rev = rev * 10 + (temp % 10);
+    temp = Math.floor(temp / 10);
+  }
+
+  return rev === n;
+};
+
+console.log(isPalNum(121));

d0740706cfd657f43866cccf3503956463e736ba
jitesh <iamjiteshsaini2004@gmail.com>
2026-02-24 00:27:29 +0530

    updated of 23-02-26

--- 23 feb 26/script2.js
+++ 23 feb 26/script2.js
@@ -1,3 +1,5 @@
+// Given a string, count the number of words in it. A word is defined as a sequence of non-space characters. The string may contain leading or trailing spaces, and there may be multiple spaces between words.
+
 const str = "     hi this          side               prajjal       ";
 const ans = str
   .trim()

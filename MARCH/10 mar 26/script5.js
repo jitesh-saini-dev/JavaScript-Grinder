a502ae561e99f03862fdf70315a11e324241b7f0
jitesh <iamjiteshsaini2004@gmail.com>
2026-03-10 23:44:14 +0530

    file updated of 10-03-26

--- MARCH/10 mar 26/script5.js
+++ MARCH/10 mar 26/script5.js
@@ -1,5 +1,25 @@
+// const patients = [
+//   { name: "John", meds: ["Paracetamol", "Ibuprofen"] },
+//   { name: "Alice", meds: ["Amoxicillin"] },
+//   { name: "Michael", meds: ["Metformin", "Atorvastatin"] },
+//   { name: "Elena", meds: ["Lisinopril"] },
+//   { name: "David", meds: ["Levothyroxine", "Vitamin D3", "Paracetamol"] },
+//   { name: "Sophia", meds: ["Albuterol", "Fluticasone"] },
+//   { name: "James", meds: ["Amlodipine", "Hydrochlorothiazide"] },
+//   { name: "Isabella", meds: ["Sertraline"] },
+//   { name: "Robert", meds: ["Gabapentin", "Naproxen", "Omeprazole"] },
+//   { name: "Mia", meds: ["Losartan"] },
+//   { name: "William", meds: ["Warfarin"] },
+//   { name: "Charlotte", meds: ["Prednisone", "Azithromycin"] },
+// ];
+// let ansset = [...new Set(patients.flatMap((x) => x.meds))];
+// let ans = patients.flatMap((x) => x.meds);
+// console.log(ans);
+
+//------------------------------------------------------------------------------
+
 const patients = [
-  { name: "John", meds: ["Paracetamol", "Ibuprofen"] },
+  { name: "John", meds: ["Paracetamol", "Ibuprofen", "Paracetamol"] },
   { name: "Alice", meds: ["Amoxicillin"] },
   { name: "Michael", meds: ["Metformin", "Atorvastatin"] },
   { name: "Elena", meds: ["Lisinopril"] },
@@ -14,4 +34,4 @@ const patients = [
 ];
 let ansset = [...new Set(patients.flatMap((x) => x.meds))];
 let ans = patients.flatMap((x) => x.meds);
-console.log(ans);
+console.log(ansset);
